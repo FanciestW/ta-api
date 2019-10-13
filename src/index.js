7adcba14f25dc01cd4dd65c1b5a2e1a18a854932
William Lin <wlin26@yahoo.com>
2019-10-13 11:07:41 -0400

    Added logic to test locally and deploy on lambda

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-module.exports.handler = serverless(app);
+app.get('/test', function (req, res){
+  res.send('The test works');
+})
+
+
+const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
+if (isInLambda) {
+  module.exports.handler = serverless(app);
+} else {
+    app.listen(3000, () => console.log(`Listening on 3000`));
+}
