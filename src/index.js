615097a3b53863e6ab2f3b410b139dac74393752
William Lin <wlin26@yahoo.com>
2019-10-13 11:16:31 -0400

    Added eslint to project

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.get('/test', function (req, res){
-  res.send('The test works');
-})
+    res.send('The test works');
+});
 
 
 const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
 if (isInLambda) {
-  module.exports.handler = serverless(app);
+    module.exports.handler = serverless(app);
 } else {
-    app.listen(3000, () => console.log(`Listening on 3000`));
+    app.listen(3000, () => console.log('Listening on 3000'));
 }
