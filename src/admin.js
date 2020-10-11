d6563a1ec6ce9a915a5c368191e71666f9407e1c
William Lin <wlin26@yahoo.com>
2020-10-11 19:49:01 -0400

    Added connection to mongo via mongoose

--- src/admin.js
+++ src/admin.js
@@ -1,6 +1,24 @@
+require('dotenv').config();
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Announcement = require('./models/Announcement.model');
+
+const mongoUser = process.env.ENV === 'prod' ? process.env.MONGODB_ANNOUNCEMENT_READ_WRITE_USER : process.env.MONGODB_ANNOUNCEMENT_DEV_READ_WRITE_USER;
+const mongoPW = process.env.ENV == 'prod' ? process.env.MONGODB_ANNOUNCEMENT_READ_WRITE_PW : process.env.MONGODB_ANNOUNCEMENT_DEV_READ_WRITE_PW;
+const mongoCluster = process.env.MONGO_CLUSTER;
+const mongoDB = process.env.ENV == 'prod' ? process.env.MONGODB_DB : process.env.MONGODB_DB_DEV;
+const DB_CONNECTION_STR = `mongodb+srv://${mongoUser}:${mongoPW}@${mongoCluster}/${mongoDB}?retryWrites=true&w=majority`;
+const mongoDBOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(DB_CONNECTION_STR, mongoDBOptions).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(() => {
+  console.log('Failed to connect to MongoDB');
+});
 
 if (!process.env.SERVERLESS) {
   const port = process.env.PORT || 8080;
