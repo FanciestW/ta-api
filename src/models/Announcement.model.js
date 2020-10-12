e7ed4de3423a3e560db525b70fa4868fadfca34a
William Lin <wlin26@yahoo.com>
2020-10-11 21:29:29 -0400

    Added comment for mongoose default

--- src/models/Announcement.model.js
+++ src/models/Announcement.model.js
@@ -7,7 +7,7 @@ const AnnouncementSchema = new Schema({
   title: { type: String, required: true },
   message: { type: String, default: ''},
   priority: { type: Number, default: 0, min: 0, max: 2 },
-  expires: { type: Date, required: false, default: () => new Date(+new Date() + 1*24*60*60*1000) },
+  expires: { type: Date, required: false, default: () => new Date(+new Date() + 1*24*60*60*1000) }, // Default: next day
   starts: {type: Date, required: false, default: Date.now },
 }, { collection: 'announcements' });
 
