650345153309895851d45b562fd6478854cbb6df
William Lin <wlin26@yahoo.com>
2020-10-11 21:28:34 -0400

    Updated announcement model

--- src/models/Announcement.model.js
+++ src/models/Announcement.model.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const AnnouncementSchema = new Schema({
-  announcementId: ObjectId,
-  title: String,
+  announcementId: {type: ObjectId, unique: true},
+  title: { type: String, required: true },
   message: { type: String, default: ''},
-  priority: { type: Number, default: 0 },
-  expires: Date,
-  starts: Date,
+  priority: { type: Number, default: 0, min: 0, max: 2 },
+  expires: { type: Date, required: false, default: () => new Date(+new Date() + 1*24*60*60*1000) },
+  starts: {type: Date, required: false, default: Date.now },
 }, { collection: 'announcements' });
 
 module.exports = mongoose.model('Announcement', AnnouncementSchema);
