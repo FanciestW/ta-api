38ae259841275f3291e65a4081e613b4d4d4f778
William Lin <wlin26@yahoo.com>
2020-10-11 20:42:07 -0400

    Created admin get annoucement endpoint

--- src/admin.js
+++ src/admin.js
@@ -4,11 +4,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Announcement = require('./models/Announcement.model');
+const { handleInternalError } = require('./utils/errorHandler');
 
 const mongoUser = process.env.ENV === 'prod' ? process.env.MONGODB_ANNOUNCEMENT_READ_WRITE_USER : process.env.MONGODB_ANNOUNCEMENT_DEV_READ_WRITE_USER;
 const mongoPW = process.env.ENV == 'prod' ? process.env.MONGODB_ANNOUNCEMENT_READ_WRITE_PW : process.env.MONGODB_ANNOUNCEMENT_DEV_READ_WRITE_PW;
 const mongoCluster = process.env.MONGO_CLUSTER;
-const mongoDB = process.env.ENV == 'prod' ? process.env.MONGODB_DB : process.env.MONGODB_DB_DEV;
+const mongoDB = process.env.ENV == 'prod' ? process.env.MONGODB_DB || 'ta' : process.env.MONGODB_DB_DEV || 'ta-dev';
 const DB_CONNECTION_STR = `mongodb+srv://${mongoUser}:${mongoPW}@${mongoCluster}/${mongoDB}?retryWrites=true&w=majority`;
 const mongoDBOptions = {
   useNewUrlParser: true,
@@ -16,11 +17,21 @@ const mongoDBOptions = {
 };
 mongoose.connect(DB_CONNECTION_STR, mongoDBOptions).then(() => {
   console.log('Connected to MongoDB');
-}).catch(() => {
-  console.log('Failed to connect to MongoDB');
+}).catch((err) => {
+  console.log(`Failed to connect to MongoDB with error: ${err}`);
 });
 
-if (!process.env.SERVERLESS) {
+app.get('/api/announcements', (req, res) => {
+  Announcement.find({$and: [{expires: {$gt: Date.now()}}, { $or: [ {starts: {$lt: Date.now()}}, {starts: null}]}]}, null, {sort: {priority: 1 }}, (err, docs) => {
+    if (err) {
+      console.log('Error occurred in fetching announcements.');
+      return handleInternalError(req, res, 'Error Fetching Announcements', err);
+    }
+    res.status(200).send(JSON.stringify({announcements: docs}));
+  });
+});
+
+if (!process.env.SERVERLESS && !process.env.IS_OFFLINE) {
   const port = process.env.PORT || 8080;
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
